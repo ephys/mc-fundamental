d25c1e1aaafa95cfa0c4db92270edd46a0758440
ephys <zoe@ephys.dev>
2022-07-21 11:57:30 +0200

    feat: add fox painting

--- src/main/java/be/ephys/fundamental/Mod.java
+++ src/main/java/be/ephys/fundamental/Mod.java
@@ -3,6 +3,7 @@ package be.ephys.fundamental;
 import be.ephys.cookiecore.config.ConfigSynchronizer;
 import be.ephys.fundamental.moss.MossModule;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.decoration.Motive;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -15,6 +16,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -29,6 +31,9 @@ public class Mod {
   public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
   public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
   public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, MODID);
+  public static final DeferredRegister<Motive> PAINTINGS = DeferredRegister.create(ForgeRegistries.PAINTING_TYPES, MODID);
+
+  public static final RegistryObject<Motive> FOX_PAINTING = PAINTINGS.register("fox", () -> new Motive(16, 32));
 
   public Mod() {
     Map<ModConfig.Type, Pair<ConfigSynchronizer.BuiltConfig, ForgeConfigSpec>> configs = ConfigSynchronizer.synchronizeConfig();
@@ -39,6 +44,7 @@ public class Mod {
     Mod.BLOCKS.register(modEventBus);
     Mod.ITEMS.register(modEventBus);
     Mod.BLOCK_ENTITIES.register(modEventBus);
+    Mod.PAINTINGS.register(modEventBus);
 
     ForgeRegistries.RECIPE_SERIALIZERS.register(ExclusionRecipe.SERIALIZER);
     CraftingHelper.register(new ConfigRecipeCondition.Serializer(commonConfig, Mod.id("boolean_config")));
